c035a0e93f89367c847cfa64efb741560540c7d9
TLSingh1 <tai8910@gmail.com>
2025-06-01 19:57:18 -0700

    fadsfs

--- modules/wm/ags/src/widgets/StatusBar/index.tsx
+++ modules/wm/ags/src/widgets/StatusBar/index.tsx
@@ -2,60 +2,18 @@ import { Variable, bind } from "astal";
 import { App, Astal, Gtk } from "astal/gtk3";
 
 export default function StatusBar() {
-  // Placeholder workspace data - will be replaced with Hyprland IPC later
-  const workspaceData = [
-    { id: 1, hasWindows: true, active: true },
-    { id: 2, hasWindows: true, active: false },
-    { id: 3, hasWindows: false, active: false },
-    { id: 4, hasWindows: true, active: false },
-    { id: 5, hasWindows: false, active: false },
-  ];
-  
-  const getWorkspaceClass = (ws: typeof workspaceData[0]) => {
-    let classes = ['workspace', `ws-${ws.id}`];
-    
-    if (ws.active) {
-      classes.push('active');
-    } else if (ws.hasWindows) {
-      classes.push('occupied');
-    } else {
-      classes.push('empty');
-    }
-    
-    return classes.join(' ');
-  };
-  
+
   return (
     <window
       name="statusbar"
       className="StatusBar"
-      anchor={Astal.WindowAnchor.TOP | Astal.WindowAnchor.LEFT | Astal.WindowAnchor.BOTTOM}
+      anchor={Astal.WindowAnchor.LEFT}
       exclusivity={Astal.Exclusivity.EXCLUSIVE}
-      layer={Astal.Layer.TOP}
       application={App}
     >
-      <box className="bar-container" vertical>
-        {/* Workspaces */}
-        <box className="workspaces" vertical>
-          {workspaceData.map(ws => (
-            <button 
-              className={getWorkspaceClass(ws)}
-              onClick={() => console.log(`Workspace ${ws.id} clicked`)}
-            >
-              <box className="indicator" />
-            </button>
-          ))}
-        </box>
-        
-        {/* Spacer */}
-        <box vexpand />
-        
-        {/* System tray placeholder */}
-        <box className="system-tray" vertical>
-          <label className="tray-icon" label="🔊" />
-          <label className="tray-icon" label="🔋" />
-          <label className="tray-icon" label="📶" />
-        </box>
+      <box className="statusbar-container" orientation={1}>
+
+
       </box>
     </window>
   );
