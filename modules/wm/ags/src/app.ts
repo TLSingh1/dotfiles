4d16e717cf99455199de4a2d0a03ae182655cc8b
TLSingh1 <tai8910@gmail.com>
2025-06-01 18:02:29 -0700

    minimal styles

--- modules/wm/ags/src/app.ts
+++ modules/wm/ags/src/app.ts
@@ -1,7 +1,9 @@
 import { App } from "astal/gtk3";
 import Clock from "./widgets/Clock";
+import style from "./styles/main.scss";
 
 App.start({
+    css: style,
     main() {
     // Initialize widgets
     Clock();
