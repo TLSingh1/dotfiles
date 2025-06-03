498a60ff40c8d4e52effbff83a63334443afbc70
TLSingh1 <tai8910@gmail.com>
2025-06-03 16:44:46 -0700

    ags

--- modules/wm/ags/config/app.ts
+++ modules/wm/ags/config/app.ts
@@ -1,11 +1,16 @@
 import { App } from "astal/gtk4"
 import style from "./style.scss"
-import Bar from "./widget/Bar"
+import Bar from "./widget/bar/Bar"
 
 App.start({
     css: style,
     main() {
+        console.log("AGS starting...")
+        const monitors = App.get_monitors()
+        console.log(`Found ${monitors.length} monitors`)
+        
         // Create bar only on primary monitor for testing
-        Bar(App.get_monitors()[0])
+        const bar = Bar(monitors[0])
+        console.log("Bar created:", bar)
     },
 })
