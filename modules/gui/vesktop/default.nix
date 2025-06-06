a2fd08d178fe799da1a86f16a66bd6ad67c187cb
TLSingh1 <tai8910@gmail.com>
2025-06-06 16:36:24 -0700

    added vesktop home manager

--- modules/gui/vesktop/default.nix
+++ modules/gui/vesktop/default.nix
@@ -3,5 +3,27 @@
 {
   programs.vesktop = {
     enable = true;
+    
+    settings = {
+      appBadge = false;
+      arRPC = true;
+      customTitleBar = false;
+      hardwareAcceleration = true;
+      discordBranch = "stable";
+      minimizeToTray = true;
+      tray = true;
+    };
+
+    vencord = {
+      themes = {
+        "translucence" = ./themes/translucence.css;
+      };
+      
+      settings = {
+        enabledThemes = [
+          "translucence.css"
+        ];
+      };
+    };
   };
 }
