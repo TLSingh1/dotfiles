ae831b450bd033a0e2951b2aaa6f362457142cb1
TLSingh1 <tai8910@gmail.com>
2025-06-08 19:09:58 -0700

    attempting to salvage

--- modules/wm/quickshell/default.nix
+++ modules/wm/quickshell/default.nix
@@ -67,5 +67,50 @@
   home.shellAliases = {
     caelestia-shell = "qs -c caelestia";
     caelestia-edit = "cd ${config.xdg.configHome}/quickshell/caelestia && $EDITOR";
+    caelestia = "caelestia-quickshell";
   };
+  
+  # Create a wrapper for caelestia that handles shell commands with quickshell
+  home.packages = with pkgs; [
+    (writeScriptBin "caelestia-quickshell" ''
+      #!${pkgs.fish}/bin/fish
+      
+      # Override for caelestia shell commands to work with quickshell
+      set -l original_caelestia ${config.programs.quickshell.caelestia-scripts}/bin/caelestia
+      
+      if test "$argv[1]" = "shell" -a -n "$argv[2]"
+          set -l cmd $argv[2]
+          set -l args $argv[3..]
+          
+          switch $cmd
+              case "show" "toggle"
+                  if test -n "$args[1]"
+                      exec ${config.programs.quickshell.finalPackage}/bin/qs -c caelestia ipc call drawers $cmd $args[1]
+                  else
+                      echo "Usage: caelestia shell $cmd <drawer>"
+                      exit 1
+                  end
+              case "media"
+                  if test -n "$args[1]"
+                      set -l action $args[1]
+                      switch $action
+                          case "play-pause"
+                              exec ${config.programs.quickshell.finalPackage}/bin/qs -c caelestia ipc call mpris playPause
+                          case '*'
+                              exec ${config.programs.quickshell.finalPackage}/bin/qs -c caelestia ipc call mpris $action
+                      end
+                  else
+                      echo "Usage: caelestia shell media <action>"
+                      exit 1
+                  end
+              case '*'
+                  # For other shell commands, try the original
+                  exec $original_caelestia $argv
+          end
+      else
+          # For non-shell commands, use the original
+          exec $original_caelestia $argv
+      end
+    '')
+  ];
 }
