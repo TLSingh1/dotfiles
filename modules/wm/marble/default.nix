5cc7c45d5aa18c0d8a85a3cc27a556cfb79138e1
TLSingh1 <tai8910@gmail.com>
2025-07-07 12:53:56 -0700

    yuh

--- modules/wm/marble/default.nix
+++ modules/wm/marble/default.nix
@@ -1,21 +1,58 @@
 { config, pkgs, lib, inputs, ... }:
 
 {
-  home.packages = [ inputs.marble.packages.${pkgs.system}.default ];
+  home.packages = with pkgs; [ 
+    inputs.marble.packages.${pkgs.system}.default
+    # Add icon themes to fix missing icons
+    papirus-icon-theme
+    adwaita-icon-theme
+    pantheon.elementary-icon-theme
+    # Additional icon themes that might have the missing icons
+    numix-icon-theme
+  ];
 
-  # Optional: Enable if you want marble to start automatically
-  # You might want to disable other window managers/shells when using marble
-  # systemd.user.services.marble = {
-  #   Unit = {
-  #     Description = "Marble Shell";
-  #     After = [ "graphical-session.target" ];
-  #   };
-  #   Service = {
-  #     ExecStart = "${inputs.marble.packages.${pkgs.system}.default}/bin/marble";
-  #     Restart = "on-failure";
-  #   };
-  #   Install = {
-  #     WantedBy = [ "default.target" ];
-  #   };
-  # };
+  # Disable dunst when marble is enabled (marble has its own notification system)
+  services.dunst.enable = lib.mkForce false;
+
+  # Configure GTK icon theme
+  gtk = {
+    enable = true;
+    iconTheme = {
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
+    };
+  };
+
+  # Set icon theme search path
+  home.sessionVariables = {
+    ICONPATH = "${pkgs.papirus-icon-theme}/share/icons:${pkgs.numix-icon-theme}/share/icons:${pkgs.pantheon.elementary-icon-theme}/share/icons";
+  };
+
+  systemd.user.services.marble = {
+    Unit = {
+      Description = "Marble Shell";
+      After = [ "graphical-session.target" ];
+      # Ensure dunst is stopped before marble starts
+      Conflicts = [ "dunst.service" ];
+    };
+    Service = {
+      ExecStart = "${inputs.marble.packages.${pkgs.system}.default}/bin/marble";
+      Restart = "on-failure";
+      # Set icon theme and Hyprland environment
+      Environment = [
+        "GTK_THEME=Adwaita"
+        "ICON_THEME=Papirus-Dark"
+        "XDG_RUNTIME_DIR=/run/user/1000"
+        "GTK_ICON_THEME=Papirus-Dark:elementary:Adwaita"
+      ];
+      # Pass Hyprland environment if available
+      PassEnvironment = [
+        "HYPRLAND_INSTANCE_SIGNATURE"
+        "HYPRLAND_CMD"
+      ];
+    };
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }
